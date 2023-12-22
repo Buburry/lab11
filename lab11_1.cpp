6f87161929cc1d0065b42615e98f60de5db2724a
Buburry <Natapon7934@gmail.com>
2023-12-22 15:33:29 +0700

    lab11.1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,56 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int grade,stat;
+
+void randomgrade(){
+    grade = rand()%9;
+}
+
+void print(){
+    switch(stat){
+        case 0:
+            cout << "A";
+        break;    
+        case 1:
+            cout << "B+";
+        break;  
+        case 2:
+            cout << "B";
+        break;    
+        case 3:
+            cout << "C+";
+        break;  
+        case 4:
+            cout << "C";
+        break;    
+        case 5:
+            cout << "D+";
+        break;  
+        case 6:
+            cout << "D";
+        break;  
+        case 7:
+            cout << "F";
+        break;    
+        case 8:
+            cout << "W";
+        break;                
+    }
+}
+
+int main(){
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout <<  "You will get " ;
+    randomgrade();
+    stat = grade;
+    print();
+    cout <<  " in this 261102.";
+}
